982a508af01ff47673bdba140477ac706598f9fa
obamium3157 <p_zhdanov@icloud.com>
2026-09-07 14:52:30 +0300

    L1T1 Написал положительный тест

--- lab1/task1/Tests/TestDance.cpp
+++ lab1/task1/Tests/TestDance.cpp
@@ -1,6 +1,57 @@
 #include <catch2/catch_test_macros.hpp>
 
-TEST_CASE("Smth")
+#include "../Duck/Duck.h"
+#include "..//Duck/Fly/FlyNoWay.h"
+#include "../Duck/Dance/IDanceBehavior.h"
+#include "../Duck/Quack/MuteQuackBehavior.h"
+
+namespace
 {
-  // ???
+  class MockDanceBehavior : public IDanceBehavior
+  {
+  public:
+    void Dance() const override
+    {
+      m_danceCount++;
+    }
+
+    unsigned int GetDanceCount() const
+    {
+      return m_danceCount;
+    }
+
+  private:
+    mutable unsigned int m_danceCount = 0;
+  };
+
+  class DancingDuck : public Duck
+  {
+  public:
+    DancingDuck(std::unique_ptr<MockDanceBehavior>&& danceBehavior)
+      : Duck(std::make_unique<MuteQuackBehavior>(),
+             std::make_unique<FlyNoWay>(),
+             std::move(danceBehavior))
+    {
+    }
+
+    void Display() const override { }
+  };
+}
+
+TEST_CASE("A dancing duck can dance")
+{
+  auto danceBehavior = std::make_unique<MockDanceBehavior>();
+  const auto* danceBehaviorRef = danceBehavior.get();
+
+  DancingDuck duck(std::move(danceBehavior));
+
+  CHECK(danceBehaviorRef->GetDanceCount() == 0);
+  duck.PerformDance();
+  CHECK(danceBehaviorRef->GetDanceCount() == 1);
+
+  for (int i = 0; i < 5; ++i)
+  {
+    duck.PerformDance();
+  }
+  CHECK(danceBehaviorRef->GetDanceCount() == 6);
 }
